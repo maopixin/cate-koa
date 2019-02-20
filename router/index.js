4a8587c23d31b00bf9ffab919537ed44955c13b4
maopixin <18364076997@163.com>
2019-02-20 15:13:57 +0800

    路由配置示例

--- router/index.js
+++ router/index.js
@@ -1,3 +1,13 @@
+// const fn_example = async ctx=> {
+//     ctx.body = 1
+// }
 
-
-module.exports = []
+// 这种配置方式更易于扩展
+// module.exports = [
+//     {
+//         type: "GET", // 请求方法（必填）
+//         url: "/example", // 请求路径（必填）
+//         middleware: fn_login, // 中间件集合 可以直接是一个函数或者多个函数的的集合[] (必填)
+//         isLogin: false, // 是否需要登录 （选填，默认值false）  
+//     }
+// ]
